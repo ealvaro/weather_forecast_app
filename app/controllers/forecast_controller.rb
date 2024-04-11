a12f4bb381304af8537d88ca7e1f7534b38216a4
Alvaro Escobar <alvaroescobaronline@gmail.com>
2024-04-10 23:43:24 -0400

    DRY up /forecast?address endpoint

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -14,33 +14,41 @@ class ForecastController < ApplicationController
   private
 
   def fetch_forecast(address)
-    # Fetch forecast data from API
     weather_data = fetch_weather_data(address)
-
-    # Cache forecast data for 30 minutes
-    Rails.cache.fetch("forecast_#{address}", expires_in: 30.minutes) do
-      weather_data.merge(cached: false) # Indicate that the data was not pulled from cache
-    end
+    cache_forecast(address, weather_data)
   end
 
   def fetch_weather_data(address)
-    # http://api.openweathermap.org/geo/1.0/direct?limit=1&q=Davie,FL,US&appid=fbe6c738c1118e617d9495aa2b858bd3
-    response = HTTParty.get("http://api.openweathermap.org/geo/1.0/direct?limit=1&q=#{address}&appid=fbe6c738c1118e617d9495aa2b858bd3")
-    response = JSON.parse(response.body)
-    # http://api.openweathermap.org/data/2.5/weather?lat=26.0628665&lon=-80.2331038&appid=fbe6c738c1118e617d9495aa2b858bd3&units=imperial
-    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=#{response[0]['lat']}&lon=#{response[0]['lon']}&appid=#{OPENWEATHERMAP_API_KEY}&units=imperial")
+    location = fetch_location(address)
+    return { error: 'Unable to fetch location data' } unless location
 
-    if response.code == 200
-      weather = JSON.parse(response.body)
-      {
-        address:,
-        temperature: weather['main']['temp'],
-        high: weather['main']['temp_max'],
-        low: weather['main']['temp_min'],
-        cached: false
-      }
-    else
-      { error: 'Unable to fetch weather data' }
+    weather = fetch_weather(location['lat'], location['lon'])
+    return { error: 'Unable to fetch weather data' } unless weather
+
+    {
+      address: address,
+      temperature: weather['main']['temp'],
+      high: weather['main']['temp_max'],
+      low: weather['main']['temp_min'],
+      cached: false
+    }
+  end
+
+  def fetch_location(address)
+    response = HTTParty.get("http://api.openweathermap.org/geo/1.0/direct?limit=1&q=#{address}&appid=#{OPENWEATHERMAP_API_KEY}")
+    JSON.parse(response.body).first
+  end
+
+  def fetch_weather(latitude, longitude)
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=#{latitude}&lon=#{longitude}&appid=#{OPENWEATHERMAP_API_KEY}&units=imperial")
+    return nil unless response.code == 200
+
+    JSON.parse(response.body)
+  end
+
+  def cache_forecast(address, weather_data)
+    Rails.cache.fetch("forecast_#{address}", expires_in: 30.minutes) do
+      weather_data.merge(cached: true)
     end
   end
 end
